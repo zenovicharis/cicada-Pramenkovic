41e27d0abdc3b9b6b151f412efb6e3243921eb2a
Haris Zenovic <zenovicharis@live.com>
2017-05-17 20:05:54 +0200

    swipable functionality added

--- front-end/resources/js/script.js
+++ front-end/resources/js/script.js
@@ -36,3 +36,40 @@ function showContact(e){
     $('.boutUsWrapper').css({'margin-left':e});
     return false;
 }
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();  
+
+    $("#carousel-example-generic").swiperight(function() {  
+        $("#carousel-example-generic").carousel('prev');  
+    });  
+    $("#carousel-example-generic").swipeleft(function() {  
+        $("#carousel-example-generic").carousel('next');  
+    });  
+
+    $("#carousel-example-generic-gallery").swiperight(function() {  
+        $("#carousel-example-generic-gallery").carousel('prev');  
+    });  
+    $("#carousel-example-generic-gallery").swipeleft(function() {  
+        $("#carousel-example-generic-gallery").carousel('next');  
+    }); 
+
+
+    $("#carousel-example-generic-mobile").swiperight(function() {  
+        $("#carousel-example-generic-mobile").carousel('prev');  
+    });  
+    $("#carousel-example-generic-mobile").swipeleft(function() {  
+        $("#carousel-example-generic-mobile").carousel('next');  
+    });
+
+    $("#carousel-example-generic-gallery-mobile").swiperight(function() {  
+        $("#carousel-example-generic-gallery-mobile").carousel('prev');  
+    });  
+    $("#carousel-example-generic-gallery-mobile").swipeleft(function() {  
+        $("#carousel-example-generic-gallery-mobile").carousel('next');  
+    });  
+});
+
+$('.hamburger-menu').on('click',function(){
+    $('.menu').slideToggle();
+});
